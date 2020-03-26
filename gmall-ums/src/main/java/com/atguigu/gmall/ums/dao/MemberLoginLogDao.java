a87944aef717459c024a969bbf88dfcebb05398a
LinHuanTan <ly.lin@yatsenglobal.com>
2020-03-26 17:45:38 +0800

    init project

---
+++ gmall-ums/src/main/java/com/atguigu/gmall/ums/dao/MemberLoginLogDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.ums.dao;
+
+import com.atguigu.gmall.ums.entity.MemberLoginLogEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 会员登录记录
+ * 
+ * @author lixianfeng
+ * @email lxf@atguigu.com
+ * @date 2020-03-26 17:28:14
+ */
+@Mapper
+public interface MemberLoginLogDao extends BaseMapper<MemberLoginLogEntity> {
+	
+}
