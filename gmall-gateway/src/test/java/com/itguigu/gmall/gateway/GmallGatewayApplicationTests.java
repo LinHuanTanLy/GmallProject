a76a5d676f9023e98ef315105729da02580c9d75
LinHuanTan <ly.lin@yatsenglobal.com>
2020-03-26 22:47:14 +0800

    修改gateway start版本不一致导致gateway网关失败

--- gmall-gateway/src/test/java/com/itguigu/gmall/gateway/GmallGatewayApplicationTests.java
+++ gmall-gateway/src/test/java/com/itguigu/gmall/gateway/GmallGatewayApplicationTests.java
@@ -1,12 +1,10 @@
 package com.itguigu.gmall.gateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallGatewayApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
